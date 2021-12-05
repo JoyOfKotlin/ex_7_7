4de41597008b0cd9587ecae8f4d8a87e111345a7
Denis Manikhin <dmanikhine@gmail.com>
2021-12-05 13:09:47 +0100

    Add example

--- app/src/main/kotlin/ex_7_7/App.kt
+++ app/src/main/kotlin/ex_7_7/App.kt
@@ -3,6 +3,106 @@
  */
 package ex_7_7
 
+
+import java.io.IOException
+import java.io.Serializable
+
+
+sealed class Result<out A>: Serializable {
+    abstract fun <B> map(f: (A)->B): Result<B>
+    abstract fun <B> flatMap(f:(A)->Result<B>): Result <B>
+    abstract fun mapFailure(message:String): Result<A>
+
+    fun filter(p:(A)-> Boolean): Result<A> =
+        flatMap {
+            if (p(it))  this
+            else failure("Condition not matched")
+        }
+
+    fun filter(message: String, p: (A)-> Boolean): Result<A> =
+        flatMap {
+            if (p(it)) this
+            else failure(message)
+        }
+
+    fun exists(p: (A) -> Boolean): Boolean = map(p).getOrElse(false)
+
+
+
+    fun getOrElse(defaultValue: @UnsafeVariance A): A = when (this) {
+        is Success -> this.value
+        else -> defaultValue
+    }
+
+    fun getOrElse(defaultValue:()->@UnsafeVariance A) : A = when
+                                                                    (this) {
+        is Success ->this.value
+        else -> defaultValue()
+    }
+
+    fun  orElse(defaultValue: ()->Result<@UnsafeVariance A>) =
+        when (this) {
+            is Success -> this
+            else -> try {
+                defaultValue()
+            }catch (e:RuntimeException) {
+                Result.failure<A>(e)
+            }catch (e:Exception) {
+                Result.failure<A>(RuntimeException(e))
+            }
+        }
+
+    internal object Empty: Result<Nothing>(){
+        override fun mapFailure(message: String) : Result<Nothing> = this
+        override fun <B> map(f: (Nothing)->B): Result<B> = Empty
+        override fun <B> flatMap(f:(Nothing)->Result<B>) : Result<B> = Empty
+        override fun toString(): String = "Empty"
+    }
+
+    internal class Failure<out A>(internal val exception: RuntimeException): Result<A>() {
+        override fun mapFailure(message: String): Result <A> =
+            Failure(RuntimeException(message,exception))
+
+        override fun <B> map(f:(A)->B): Result<B> = Failure(exception)
+        override fun <B> flatMap(f:(A)->Result<B>) : Result<B> = Failure(exception)
+
+
+        override fun toString(): String="Failure (${exception.message})"
+    }
+
+    internal class Success<out A> (internal val value: A) : Result<A>() {
+        override fun mapFailure(message: String) : Result<A> = this
+        override fun <B> map(f: (A) -> B): Result<B> =
+            try {
+                Success(f(value))
+            } catch (e:RuntimeException) {
+                Failure(e)
+            } catch (e: Exception) {
+                Failure(RuntimeException(e))
+            }
+
+        override fun <B>    flatMap (f:(A)->Result<B>): Result<B> =
+            try {
+                f(value)
+            } catch (e: RuntimeException) {
+                Failure(e)
+            } catch (e:Exception) {
+                Failure (RuntimeException (e))
+            }
+        override fun toString():String="Success($value)"
+    }
+
+    companion object {
+        operator fun <A> invoke(a: A? = null): Result<A> = when (a) {
+            null -> Failure(NullPointerException())
+            else -> Success(a)
+        }
+        operator fun <A> invoke (): Result<A> = Empty
+        fun <A> failure(message : String) :             Result<A> =  Failure(IllegalStateException(message))
+        fun <A> failure (exception: RuntimeException):  Result<A> = Failure(exception)
+        fun <A> failure (exception: Exception):         Result<A> =  Failure(IllegalStateException(exception))
+    }
+}
 class App {
     val greeting: String
         get() {
@@ -12,4 +112,8 @@ class App {
 
 fun main() {
     println(App().greeting)
+    val r0=Result(0)
+    val _failure=r0.filter { (10/it)>0 }
+    println(_failure)
+    println(_failure.mapFailure("divizion by zero"))
 }
